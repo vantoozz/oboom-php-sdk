dcbad17f900d7f0a62ce30ded2d6af0aa5eeef83
Ivan Nikitin <nikitin@coccoc.com>
2014-12-26 12:03:43 +0700

    + login

--- ci/phpspec/spec/Vantoozz/Oboom/APISpec.php
+++ ci/phpspec/spec/Vantoozz/Oboom/APISpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Vantoozz\Oboom;
 
+use InvalidArgumentException;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -11,4 +12,44 @@ class APISpec extends ObjectBehavior
     {
         $this->shouldHaveType('Vantoozz\Oboom\API');
     }
+
+    function it_sets_config()
+    {
+        $this->setConfig([
+            'username' => 'username',
+            'password' => 'password',
+            'source'   => 'source'
+        ])->shouldReturn($this);
+    }
+
+    function it_requires_user_config_param()
+    {
+        $this
+            ->shouldThrow(new InvalidArgumentException('No username given'))
+            ->during('setConfig', [['source' => 'source', 'password' => 'password']]);
+    }
+
+    function it_requires_password_config_param()
+    {
+        $this
+            ->shouldThrow(new InvalidArgumentException('No password given'))
+            ->during('setConfig', [['source' => 'source', 'username' => 'username']]);
+    }
+
+    function it_requires_source_config_param()
+    {
+        $this
+            ->shouldThrow(new InvalidArgumentException('No source given'))
+            ->during('setConfig', [['username' => 'username', 'password' => 'password']]);
+    }
+
+    function it_logins()
+    {
+        $this->setConfig([
+            'username' => 'vantoozz@gmail.com',
+            'password' => 'bc5e6b6e',
+            'source'   => '200'
+        ])->login()->shouldReturn($this);
+    }
+
 }
--- src/Vantoozz/Oboom/API.php
+++ src/Vantoozz/Oboom/API.php
@@ -2,10 +2,119 @@
 
 namespace Vantoozz\Oboom;
 
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use InvalidArgumentException;
+use RuntimeException;
+
 /**
  * Class API
  * @package Vantoozz\Oboom
  */
 class API
 {
+    const ENDPOINT_WWW = 'www.oboom.com';
+
+    /**
+     * @var string
+     */
+    private $username;
+    /**
+     * @var string
+     */
+    private $password;
+    /**
+     * @var string
+     */
+    private $source;
+
+    /**
+     * @var string
+     */
+    private $session;
+
+    /**
+     * @var Client
+     */
+    private $guzzle;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->guzzle = new Client();
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return $this
+     */
+    public function setConfig(array $config)
+    {
+        foreach (array('username', 'password', 'source') as $param) {
+            if (empty($config[$param])) {
+                throw new InvalidArgumentException('No ' . $param . ' given');
+            }
+            $this->$param = $config[$param];
+        }
+
+        $this->password = $this->hashPassword($this->password);
+
+        return $this;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return string
+     */
+    private function hashPassword($password)
+    {
+        return hash_pbkdf2('sha1', $password, strrev($password), 1000, 16);
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function login()
+    {
+        $data = $this->call('/1/login', [
+            'auth'   => $this->username,
+            'pass'   => $this->password,
+            'source' => $this->source
+        ]);
+
+        $this->session = $data['session'];
+
+        return $this;
+    }
+
+    /**
+     * @param $method
+     * @param $params
+     *
+     * @return mixed
+     */
+    public function call($method, $params)
+    {
+
+        try {
+            $response = $this->guzzle->get('http://' . self::ENDPOINT_WWW . $method, [
+                'query' => $params
+            ]);
+        } catch (Exception $e) {
+            throw new RuntimeException($e->getMessage());
+        }
+
+        $response = $response->json();
+
+        if (200 !== $response[0]) {
+            throw new RuntimeException($response[1]);
+        }
+        return $response[1];
+    }
 }
